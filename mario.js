a895ec81dfbcb588003d1cc24e7108b0e866e4b2
Nick Castagnaro <nickshs123@gmail.com>
2021-02-07 14:10:28 -0500

    init

---
+++ mario.js
@@ -0,0 +1,27 @@
+let height =8;
+let x = "x";       //set a variable for x
+let space = "\xa0"; //This allows you to create a variable for a space
+
+//We start at i=1, so that way when we include the spaces for each row, it is just height-i, which is neater than if we did height-i-1, if we started at i=0
+//To keep the x on the same line we just added it. To repeat a certain amount of times you need to use the repeat method. You cannot say space*height-i.
+for(let i = 1; i <= height;i++){
+        console.log(space.repeat(height-i)+x.repeat(i));
+    }
+
+    //Output:
+
+    //       X
+    //      XX
+    //     XXX
+    //    XXXX
+    //   XXXXX
+    //  XXXXXX
+    // XXXXXXX
+    //XXXXXXXX
+
+
+
+
+
+
+
