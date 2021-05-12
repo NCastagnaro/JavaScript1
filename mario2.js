67be8bba2838708299444df73eb306726e3ff53e
Nick Castagnaro <nickshs123@gmail.com>
2021-05-12 15:36:50 -0400

    Update mario2.js

--- mario2.js
+++ mario2.js
@@ -1,7 +1,7 @@
 let height =8;
 let x = "x";        //set a variable for x.
 let space = "\xa0"; //This allows you to create a variable for a space.
-let space2 = "\xa0\xa0" //This allows you to create the double space in the middle 
+let space2 = "\xa0\xa0" //This allows you to create the double space in the middle. 
 
 //We start at i=1, so that way when we include the spaces for each row, it is just height-i, which is neater than if we did height-i-1, if we started at i=0
 //To keep the x on the same line we just added it. To repeat a certain amount of times you need to use the repeat method. You cannot say space*height-i.
