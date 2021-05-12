7a779c5873ee06c7c59f202be68cd82118ba8ada
Nick Castagnaro <nickshs123@gmail.com>
2021-05-12 15:36:41 -0400

    Update mario2.js

--- mario2.js
+++ mario2.js
@@ -1,6 +1,6 @@
 let height =8;
 let x = "x";        //set a variable for x.
-let space = "\xa0"; //This allows you to create a variable for a space
+let space = "\xa0"; //This allows you to create a variable for a space.
 let space2 = "\xa0\xa0" //This allows you to create the double space in the middle 
 
 //We start at i=1, so that way when we include the spaces for each row, it is just height-i, which is neater than if we did height-i-1, if we started at i=0
