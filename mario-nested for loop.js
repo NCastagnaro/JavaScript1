d82616a0e51ab977bf2772b103a0f7fc66821cae
Nick Castagnaro <nickshs123@gmail.com>
2021-05-17 12:34:01 -0400

    Add files via upload

---
+++ mario-nested for loop.js
@@ -0,0 +1,24 @@
+
+function mario(h){
+    let space = " ";
+    let star = "*";
+    for (let i=0; i<h; i++){
+        let z = (space.repeat(h-i-1));
+      for(let j = 0; j <1; j++){
+          console.log(z + star.repeat(i+1));
+      }
+    }
+}
+mario(8);
+
+
+
+//h = 8
+//       *
+//      **
+//     ***
+//    ****
+//   *****
+//  ******
+// *******
+//********
